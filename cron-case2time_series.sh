e5687dced044ad7707eb0af7959cf9d6bd717e7d
Yoshiro MIHIRA <sanpei@sanpei.org>
2020-04-05 09:46:18 +0900

    add cron.sh

---
+++ cron-case2time_series.sh
@@ -0,0 +1,49 @@
+#!/usr/local/bin/bash
+CSV_FILE="time_series_covid19_confirmed_Japan.csv"
+WORKING_PATH=/home/sanpei/src/covidtrends/converter
+to_address="sanpei@sanpei.org"
+COVID_CSV_FILE="COVID-19.csv"
+URL="https://dl.dropboxusercontent.com/s/6mztoeb6xf78g5w/COVID-19.csv"
+
+CURL="/usr/local/bin/curl"
+FETCH="/usr/bin/fetch"
+MAIL="/usr/bin/mail"
+
+cd ${WORKING_PATH}
+git pull
+if [ $? -ne 0 ]; then
+    echo update ${CSV_FILE} | ${MAIL} -s "covid19 git pull error" ${to_address}
+    exit
+fi
+
+#${CURL} -o ${COVID_CSV_FILE} ${URL}
+# currently I cannot find the way to get error code. so I use fetch in FreeBSD
+${FETCH} ${URL}
+if [ $? -ne 0 ]; then
+    echo update ${CSV_FILE} | ${MAIL} -s "covid19 download ERROR" ${to_address}
+    exit
+fi
+if [ ! -s ${COVID_CSV_FILE} ]; then
+    echo update ${CSV_FILE} | ${MAIL} -s "covid19 download SIZE ZERO ERROR" ${to_address}
+    exit
+fi
+
+/usr/local/bin/ruby case2time_series.rb
+if [ $? -ne 0 ]; then
+    echo update ${CSV_FILE} | ${MAIL} -s "covid19 ERROR" ${to_address}
+    exit
+fi
+if [ ! -s ${CSV_FILE} ]; then
+    echo update ${CSV_FILE} | ${MAIL} -s "covid19 SIZE ZERO ERROR" ${to_address}
+    exit
+fi
+git commit -m "`/bin/date`" time_series_covid19_confirmed_Japan.csv
+if [ $? -ne 0 ]; then
+    echo update ${CSV_FILE} | ${MAIL} -s "covid19 git commit error" ${to_address}
+    exit
+fi
+git push -u origin master
+if [ $? -ne 0 ]; then
+    echo update ${CSV_FILE} | ${MAIL} -s "covid19 git push error" ${to_address}
+    exit
+fi
---
+++ cron.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+GIT="/usr/local/bin/git"
+cd /home/sanpei/src/covid19jp/CSSEGISandData/COVID-19/ ; ${GIT} pull
+cd /home/sanpei/src/covid19jp/CSSEGISandData/COVID-19/csse_covid_19_data ; ${GIT} pull
+cd /home/sanpei/src/covid19jp
+/usr/local/bin/ruby rapper.rb
+cd contents/sanpei3.github.io
+/bin/cp covid19jp-100-33.html index.html
+/bin/cp covid19jp-150-US-en.html covid19.html
+${GIT} commit -m "`/bin/date`" covid19jp-1.html covid19jp-10.html covid19jp-100.html covid19jp-20.html covid19jp-30.html covid19jp-40.html covid19jp-50.html covid19jp-60.html covid19jp-70.html covid19jp-80.html covid19jp-90.html index.html covid19jp-1-33.html covid19jp-10-33.html covid19jp-100-33.html covid19jp-20-33.html covid19jp-30-33.html covid19jp-40-33.html covid19jp-50-33.html covid19jp-60-33.html covid19jp-70-33.html covid19jp-80-33.html covid19jp-90-33.html covid19jp-1-33-en.html covid19jp-1-en.html covid19jp-10-33-en.html covid19jp-10-en.html covid19jp-100-33-en.html covid19jp-100-en.html covid19jp-20-33-en.html covid19jp-20-en.html covid19jp-30-33-en.html covid19jp-30-en.html covid19jp-40-33-en.html covid19jp-40-en.html covid19jp-50-33-en.html covid19jp-50-en.html covid19jp-60-33-en.html covid19jp-60-en.html covid19jp-70-33-en.html covid19jp-70-en.html covid19jp-80-33-en.html covid19jp-80-en.html covid19jp-90-33-en.html covid19jp-90-en.html covid19jp-150-33-en.html  covid19jp-150-en.html covid19jp-150-33.html     covid19jp-150.html covid19.html covid19jp-100-US-en.html covid19jp-100-US-ja.html covid19jp-100-WW-en.html covid19jp-100-WW-ja.html covid19jp-150-US-en.html covid19jp-150-US-ja.html covid19jp-150-WW-en.html covid19jp-150-WW-ja.html
+${GIT} push -u origin master
+cd /usr/home/sanpei/src/covid19jp
+#/usr/local/bin/ruby CSSEGISandData.rb > /home/sanpei/public_html/tmp/hoge.html
