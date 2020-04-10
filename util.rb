b971477ea24d9d9890337f3890fa407875b5ffc6
Yoshiro MIHIRA <sanpei@sanpei.org>
2020-04-10 09:06:06 +0900

    add lat long data in time_series_covid19_confirmed_Japan.csv

--- util.rb
+++ util.rb
@@ -90,8 +90,8 @@ $pref_en = {"北海道": "Hokkaido",
            "熊本県": "Kumamoto",
            "大分県": "Oita",
            "宮崎県": "Miyazaki",
-           "鹿児島県": "Kagoshima",
-           "沖縄県": "Okinawa",
+            "鹿児島県": "Kagoshima",
+            "沖縄県": "Okinawa",
             "羽田空港": "Haneda Airport",
             "成田空港": "Narita Airport",
             "関西国際空港": "Kansai Kokusai Airport",
@@ -100,6 +100,60 @@ $pref_en = {"北海道": "Hokkaido",
            "東京最大予測": "Tokyo Max",
            "東京平均予測": "Tokyo avg",
            }
+
+$pref_latlong = {
+  Hokkaido: [43.4672, 43.4672],
+  Aomori: [40.7803, 40.7803],
+  Iwate: [39.5914, 39.5914],
+  Miyagi: [38.4456, 38.4456],
+  Akita: [39.7475, 39.7475],
+  Yamagata: [38.4464, 38.4464],
+  Fukushima: [37.3789, 37.3789],
+  Ibaraki: [36.3064, 36.3064],
+  Tochigi: [36.6892, 36.6892],
+  Gunma: [36.5039, 36.5039],
+  Saitama: [35.9967, 35.9967],
+  Chiba: [35.5128, 35.5128],
+  Tokyo: [35.0183, 35.0183],
+  Kanagawa: [35.4142, 35.4142],
+  Niigata: [37.5189, 37.5189],
+  Toyama: [36.6361, 36.6361],
+  Ishikawa: [36.7658, 36.7658],
+  Fukui: [35.8467, 35.8467],
+  Yamanashi: [35.6122, 35.6122],
+  Nagano: [36.13, 36.13],
+  Gifu: [35.7775, 35.7775],
+  Shizuoka: [35.0169, 35.0169],
+  Aichi: [35.0344, 35.0344],
+  Mie: [34.5136, 34.5136],
+  Shiga: [35.2153, 35.2153],
+  Kyoto: [35.2519, 35.2519],
+  Osaka: [34.6228, 34.6228],
+  Hyogo: [35.0369, 35.0369],
+  Nara: [34.3156, 34.3156],
+  Wakayama: [33.9094, 33.9094],
+  Tottori: [35.3606, 35.3606],
+  Shimane: [35.0731, 35.0731],
+  Okayama: [34.9008, 34.9008],
+  Hiroshima: [34.6036, 34.6036],
+  Yamaguchi: [34.1986, 34.1986],
+  Tokushima: [33.9181, 33.9181],
+  Kagawa: [34.2431, 34.2431],
+  Ehime: [33.6219, 33.6219],
+  Kochi: [33.4211, 33.4211],
+  Fukuoka: [33.5225, 33.5225],
+  Saga: [33.2853, 33.2853],
+  Nagasaki: [33.2275, 33.2275],
+  Kumamoto: [32.615, 32.615],
+  Oita: [33.1992, 33.1992],
+  Miyazaki: [32.1908, 32.1908],
+  Kagoshima: [31.0128, 31.0128],
+  Okinawa: [25.7711, 25.7711],
+  "Haneda Airport": [35.5448, 139.7672],
+  "Narita Airport": [35.7719, 140.3928],
+  "Kansai Kokusai Airport": [34.4361, 135.2432],
+  "Chubu Kokusai Airport": [34.8592, 136.8163],
+}
 
 $color_table = [ #"Red",
   "Blue", "Green", "Black", #"Cyan",
@@ -132,5 +186,13 @@ def prefJa2prefEn (pref, lang)
       return $pref_en[:"#{pref}"]
   else
     return pref
+  end
+end
+
+def pref2latlong (pref)
+  if ($pref_latlong[:"#{pref}"] != nil)
+    return $pref_latlong[:"#{pref}"]
+  else
+    return [0, 0]
   end
 end
