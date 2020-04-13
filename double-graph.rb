3eb3726c49f11cbb89af7a143655506bb58ad788
Yoshiro MIHIRA <sanpei@sanpei.org>
2020-04-13 10:47:42 +0900

    add countres_flag and state_flag

--- double-graph.rb
+++ double-graph.rb
@@ -9,11 +9,12 @@ color_index = 0
 # XX 海外の国や、US 州を出してみる
 
 lang = "-en"
-
+countres_flag = nil
+states_flag = nil
 states = []
 countries = []
 
-if (nil)
+if (states_flag)
   states = [
     ["Minnesota", "US"],
     ["New York", "US"],
@@ -21,7 +22,7 @@ if (nil)
     ["Washington", "US"]
   ]
 end
-if (nil)
+if (countres_flag)
   countries = ["US", "Italy", "Spain", "Korea, South", "United Kingdom", "India", "Serbia", "Germany", "Philippines"]
 end
 prefectures = {"Tokyo" => true,
@@ -31,7 +32,13 @@ prefectures = {"Tokyo" => true,
                "Osaka" => true,
                "Hyogo" => true,
                "Fukuoka" => true,
-               "Aichi" => true
+               "Aichi" => true,
+               "Hokkaido" => true,
+               "Ibaraki" => true,
+               "Okinawa" => true,
+               "Toyama" => true,
+               "Gunma" => true,
+               "Ishikawa" => true,
               }
 
 ########################################
@@ -73,88 +80,89 @@ if (/(\d+)\/(\d\d)\/(\d\d)/ =~ start_date)
 end
 
 
-base_count = 1
-last_day = {}
-dir_name = "CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"
-Dir::foreach(dir_name) do |filename|
-  if (filename =~ /\.csv$/)
-    onedayData = {}
-    date = ""
-    CSV.foreach(dir_name + filename, "r:UTF-8") do |row|
-      /(\d\d)-(\d\d)-(\d\d\d\d).csv/.match(filename)
-      date = Date.new($3.to_i, $1.to_i, $2.to_i)
-      if (date >= start_Date - 6)
-        if (olderThan03212020(filename))
-          state = row[0]
-          country = row[1]
-          confirmed = row[3].to_i
-          if (confirmed.to_i >= base_count)
+if (states_flag)
+  base_count = 1
+  last_day = {}
+  dir_name = "CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"
+  Dir::foreach(dir_name) do |filename|
+    if (filename =~ /\.csv$/)
+      onedayData = {}
+      date = ""
+      CSV.foreach(dir_name + filename, "r:UTF-8") do |row|
+        /(\d\d)-(\d\d)-(\d\d\d\d).csv/.match(filename)
+        date = Date.new($3.to_i, $1.to_i, $2.to_i)
+        if (date >= start_Date - 6)
+          if (olderThan03212020(filename))
+            state = row[0]
+            country = row[1]
+            confirmed = row[3].to_i
+            if (confirmed.to_i >= base_count)
+              states.each{|s, c|
+                if (state == s && country == c)
+                  m_index = "#{s},#{c}"
+                  onedayData[m_index] = confirmed
+                end
+              }
+            end
+          else
+            county = row[1]
+            state = row[2]
+            country = row[3]
+            confirmed = row[7].to_i
             states.each{|s, c|
               if (state == s && country == c)
                 m_index = "#{s},#{c}"
-                onedayData[m_index] = confirmed
+                # at first s, c だったら、テンポラリの合計に入れる
+                if (onedayData[m_index]  == nil)
+                  onedayData[m_index]  = confirmed
+                else
+                  onedayData[m_index]  = onedayData[m_index] + confirmed
+                end
               end
             }
           end
-        else
-          county = row[1]
-          state = row[2]
-          country = row[3]
-          confirmed = row[7].to_i
-          states.each{|s, c|
-            if (state == s && country == c)
-              m_index = "#{s},#{c}"
-              # at first s, c だったら、テンポラリの合計に入れる
-              if (onedayData[m_index]  == nil)
-                onedayData[m_index]  = confirmed
-              else
-                onedayData[m_index]  = onedayData[m_index] + confirmed
-              end
-            end
-          }
         end
       end
+      # after March/22 check whether add to m[] or not
+      states.each{|s, c|
+        m_index = "#{s},#{c}"
+        d = date2mmdd(date)
+        if (onedayData[m_index] != nil && onedayData[m_index] >= base_count)
+          confirmed = onedayData[m_index]
+          if (last_day[m_index] == nil)
+            #puts s, c, confirmed, date.to_s
+            last_day[m_index] = 0
+            m[m_index] = []
+          else
+            last_day[m_index] = last_day[m_index] + 1
+          end
+          d_index = index2days(last_day[m_index] + 1, lang)
+          #g = [confirmed, "#{d_index}:#{d}"]
+          g = confirmed.to_i
+          m[m_index][last_day[m_index]] = g
+          if (max_x < last_day[m_index])
+            max_x = last_day[m_index]
+          end
+          if (max_y < confirmed)
+            max_y = confirmed
+          end
+        end
+      }
     end
-    # after March/22 check whether add to m[] or not
-    states.each{|s, c|
-      m_index = "#{s},#{c}"
-      d = date2mmdd(date)
-      if (onedayData[m_index] != nil && onedayData[m_index] >= base_count)
-        confirmed = onedayData[m_index]
-        if (last_day[m_index] == nil)
-          #puts s, c, confirmed, date.to_s
-          last_day[m_index] = 0
-          m[m_index] = []
-        else
-          last_day[m_index] = last_day[m_index] + 1
-        end
-        d_index = index2days(last_day[m_index] + 1, lang)
-        #g = [confirmed, "#{d_index}:#{d}"]
-        g = confirmed.to_i
-        m[m_index][last_day[m_index]] = g
-        if (max_x < last_day[m_index])
-          max_x = last_day[m_index]
-        end
-        if (max_y < confirmed)
-          max_y = confirmed
-        end
-      end
-    }
   end
+  max_x = max_x - 6
+  m.each { |a|
+    i = a[1].length - 1
+    while (i >= 6)
+      m[a[0]][i] = [calculate_double_days(a[1], i), ""]
+      i = i - 1
+    end
+    while (i >= 0)
+      m[a[0]].shift
+      i = i - 1
+    end
+  }
 end
-max_x = max_x - 6
-m.each { |a|
-  i = a[1].length - 1
-  while (i >= 6)
-    m[a[0]][i] = [calculate_double_days(a[1], i), ""]
-    i = i - 1
-  end
-  while (i >= 0)
-    m[a[0]].shift
-    i = i - 1
-  end
-}
-
 
 start_i_j = 4
 
@@ -194,45 +202,46 @@ CSV.foreach("time_series_covid19_confirmed_Japan.csv", "r:UTF-8") do |row|
   # 探したところから、7日間の値を得る
 end
 
-countries_hash = {}
-countries.each{ |c|
-  countries_hash.store(:"#{c}", true)
-}
-start_i = 4
-time_series_header_global = []
-CSV.foreach("./CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", "r:UTF-8") do |row|
-  if (time_series_header_global == [])
-    time_series_header_global = row
-    max_row = row.length
-    # start_dateな rowを探す
-    i = 4
-    while (row[i] != start_date)
-      i = i + 1
-    end
-    start_i = i
-    next
-  end
-  pref = row[1]
-  if (countries_hash[:"#{pref}"])
-    i = start_i
-    d_i = 0
-    while (i < max_row)
-      d = calculate_double_days(row, i)
-      if (m[pref] == nil)
-        m[pref] = [d, time_series_header_global[i]]
-      else
-        m[pref][d_i] = [d, time_series_header_global[i]]
+if (countres_flag)
+  countries_hash = {}
+  countries.each{ |c|
+    countries_hash.store(:"#{c}", true)
+  }
+  start_i = 4
+  time_series_header_global = []
+  CSV.foreach("./CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", "r:UTF-8") do |row|
+    if (time_series_header_global == [])
+      time_series_header_global = row
+      max_row = row.length
+      # start_dateな rowを探す
+      i = 4
+      while (row[i] != start_date)
+        i = i + 1
       end
-      i = i + 1
-      d_i = d_i + 1
-      if (max_x <= d_i)
-        max_x = d_i
+      start_i = i
+      next
+    end
+    pref = row[1]
+    if (countries_hash[:"#{pref}"])
+      i = start_i
+      d_i = 0
+      while (i < max_row)
+        d = calculate_double_days(row, i)
+        if (m[pref] == nil)
+          m[pref] = [d, time_series_header_global[i]]
+        else
+          m[pref][d_i] = [d, time_series_header_global[i]]
+        end
+        i = i + 1
+        d_i = d_i + 1
+        if (max_x <= d_i)
+          max_x = d_i
+        end
       end
     end
+    # 探したところから、7日間の値を得る
   end
-  # 探したところから、7日間の値を得る
-end
-
+end  
 
 pref = []
 colors =[]
@@ -297,5 +306,5 @@ readHtml("tail-double#{lang}#{world_wide}.html", [])
 puts "colors: #{colors}"
 
 
-readHtml("tail2#{lang}#{world_wide}.html", select_str)
+readHtml("tail2-double#{lang}#{world_wide}.html", select_str)
 
