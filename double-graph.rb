0fb9c8b9aafd2b63a678ba99d11deb102d8888fb
Yoshiro MIHIRA <sanpei@sanpei.org>
2020-05-05 20:53:18 +0900

    change japanese prefecture_list from hash to array add check illegal number support tooltips

--- double-graph.rb
+++ double-graph.rb
@@ -3,54 +3,71 @@
 require 'csv'
 require "./util"
 
+start_date = "3/15/20"
+$days = 6
+
 max_color_index = $color_table.length
 color_index = 0
 # グラフフォーマットへ変換
 # XX 海外の国や、US 州を出してみる
 
-lang = "-en"
+lang = "-ja"
+if (lang != "-en")
+  lang = ""
+end
 countres_flag = nil
-states_flag = nil
+states_flag =true
 states = []
 countries = []
 
 if (states_flag)
   states = [
     ["Minnesota", "US"],
-    ["New York", "US"],
-    ["California", "US"],
-    ["Washington", "US"]
+#    ["New York", "US"],
+#    ["California", "US"],
+#    ["Washington", "US"]
   ]
 end
 if (countres_flag)
-  countries = ["US", "Italy", "Spain", "Korea, South", "United Kingdom", "India", "Serbia", "Germany", "Philippines"]
+  countries = ["US", "Italy", "Spain", "Korea, South", "United Kingdom", "India", "Serbia", "Germany", "Philippines", "Turkey", "Russia", "Austria", "Japan"]
 end
-prefectures = {"Tokyo" => true,
-               "Kanagawa" => true,
-               "Chiba" => true,
-               "Saitama" => true,
-               "Osaka" => true,
-               "Hyogo" => true,
-               "Fukuoka" => true,
-               "Aichi" => true,
-               "Hokkaido" => true,
-               "Ibaraki" => true,
-               "Okinawa" => true,
-               "Toyama" => true,
-               "Gunma" => true,
-               "Ishikawa" => true,
-              }
+prefecture_list = ["Tokyo",
+               "Kanagawa",
+               "Chiba",
+               "Saitama",
+               "Osaka",
+               "Hyogo",
+               "Fukuoka",
+               "Aichi",
+               "Hokkaido",
+               "Ibaraki",
+               "Okinawa",
+               "Toyama",
+               "Gunma",
+               "Ishikawa",
+                  ]
+prefecture_list = [
+  "Tokyo",
+              "Hokkaido",
+              ]
+prefectures = {}
+prefecture_list.each do |p|
+  prefectures[p] = true
+end
 
 ########################################
 
 def calculate_double_days(row, i)
-  d = 6
-  double = (d * Math.log(2, 2) / Math.log(row[i].to_f / row[i - d].to_f, 2))
-  if (double == Float::INFINITY || double.nan?)
-    return 100
-  else
-    digit = 100.0
-    return ((double * digit).round)/digit
+  if (row[i] == row[i - $days] || row[i - $days].to_i == 0)
+    return Float::INFINITY
+  else    
+    double = (($days) * Math.log(2, 2) / Math.log(row[i].to_f / row[i - $days].to_f, 2))
+    if (double == Float::INFINITY || double.nan?)
+      return Float::INFINITY
+    else
+      digit = 100.0
+      return ((double * digit).round)/digit
+    end
   end
 end
 
@@ -73,7 +90,6 @@ max_row = 0
 max_x = 0
 max_y = 0
 
-start_date = "3/23/20"
 start_Date = Time.now
 if (/(\d+)\/(\d\d)\/(\d\d)/ =~ start_date)
   start_Date = Date.new("20#{$3}".to_i, $1.to_i, $2.to_i)
@@ -83,6 +99,7 @@ end
 if (states_flag)
   base_count = 1
   last_day = {}
+  days = {}
   dir_name = "CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"
   Dir::foreach(dir_name) do |filename|
     if (filename =~ /\.csv$/)
@@ -130,7 +147,6 @@ if (states_flag)
         if (onedayData[m_index] != nil && onedayData[m_index] >= base_count)
           confirmed = onedayData[m_index]
           if (last_day[m_index] == nil)
-            #puts s, c, confirmed, date.to_s
             last_day[m_index] = 0
             m[m_index] = []
           else
@@ -139,6 +155,7 @@ if (states_flag)
           d_index = index2days(last_day[m_index] + 1, lang)
           #g = [confirmed, "#{d_index}:#{d}"]
           g = confirmed.to_i
+          days[last_day[m_index]] = "#{d_index}:#{d}"
           m[m_index][last_day[m_index]] = g
           if (max_x < last_day[m_index])
             max_x = last_day[m_index]
@@ -154,7 +171,7 @@ if (states_flag)
   m.each { |a|
     i = a[1].length - 1
     while (i >= 6)
-      m[a[0]][i] = [calculate_double_days(a[1], i), ""]
+      m[a[0]][i] = [calculate_double_days(a[1], i), "#{days[i]}"]
       i = i - 1
     end
     while (i >= 0)
@@ -179,19 +196,25 @@ CSV.foreach("time_series_covid19_confirmed_Japan.csv", "r:UTF-8") do |row|
     next
   end
   pref = row[0]
-  if (prefectures[pref])
-#  if (true)
+  last_d = Float::INFINITY
+#  if (prefectures[pref])
+  if ((prefectures[pref] && row[row.length - 1].to_i > 100) &&
+      pref != "Narita Airport")
     #  puts pref
     i = start_i_j
     d_i = 0
     while ( i < max_row)
       d = calculate_double_days(row, i)
+      if (d == Float::INFINITY && last_d != Float::INFINITY)
+        d = last_d
+      end
       #    puts d
       if (m[pref] == nil)
         m[pref] = [d, time_series_header[i]]
       else
         m[pref][d_i] = [d, time_series_header[i]]
       end
+      last_d = d
       i = i + 1
       d_i = d_i + 1
       if (max_x <= d_i)
@@ -222,7 +245,7 @@ if (countres_flag)
       next
     end
     pref = row[1]
-    if (countries_hash[:"#{pref}"])
+    if (countries_hash[:"#{pref}"] && row[0].to_s == "")
       i = start_i
       d_i = 0
       while (i < max_row)
@@ -258,7 +281,7 @@ end
 
 m.each{|a|
   x = 0
-  pref.push(a[0])
+  pref.push(prefen2prefjp(a[0], lang))
   if (a[0] == "Tokyo")
       colors.push("Red")
   else
@@ -266,13 +289,28 @@ m.each{|a|
     color_index = color_index + 1
   end
   l = 0
+  infinity_flag = nil
   a[1].each {|i, d|
-    data[x].push(i)
-    data[x].push("''")
-    data[x].push("true")
-    p = a[0]
-    data[x].push("'#{d}\n#{p}:#{i}'")
-    data[x].push("null")
+    if (i == Float::INFINITY)
+      data[x].push(100)
+      data[x].push("'stroke-width: 0;'")
+      data[x].push("true")
+      data[x].push("''")
+      data[x].push("null")
+      infinity_flag = true
+    else
+      data[x].push(i)
+      if (infinity_flag)
+        data[x].push("'stroke-width: 0;'")
+        infinity_flag = nil
+      else
+        data[x].push("''")
+      end
+      data[x].push("true")
+      p = a[0]
+      data[x].push("'#{d}\n#{p}:#{i}'")
+      data[x].push("null")
+    end
       x = x + 1
   }
   if (x <= max_x)
@@ -291,8 +329,16 @@ m.each{|a|
 world_wide = ""
 select_str = []
 select_str.push(["%%UPDATE%%", Time.now.to_s])
+days_str = ["%%DAYS%%", ($days + 1).to_s]
+header_str = []
+header_str.push(days_str)
 
-readHtml("header-double#{lang}#{world_wide}.html", [])
+tail_str = []
+yscale = ['%%yscale%%', "true"]
+tail_str.push(yscale)
+tail_str.push(days_str)
+
+readHtml("header-double#{lang}#{world_wide}.html", header_str)
 
 puts "var pref =  #{pref};"
 
@@ -301,7 +347,7 @@ readHtml("mid-double.html", [])
 data_str = "data.addRows(#{data});".gsub(/"/,"")
 puts data_str
 
-readHtml("tail-double#{lang}#{world_wide}.html", [])
+readHtml("tail-double#{lang}#{world_wide}.html", tail_str)
 
 puts "colors: #{colors}"
 
