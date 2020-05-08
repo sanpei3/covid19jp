10b7ffe03b20bc836a09a788200478a573c30e2c
Yoshiro MIHIRA <sanpei@sanpei.org>
2020-05-08 20:56:51 +0900

    add support US state automatically create button

--- daily-graph.rb
+++ daily-graph.rb
@@ -9,6 +9,9 @@ states = []
 if (states_flag)
   states = [
     ["Minnesota", "US"],
+    ["New York", "US"],
+    ["California", "US"],
+    ["Washington", "US"]
   ]
 end
 prefecture_list = [
@@ -24,7 +27,7 @@ end
 ########################################
 # XX X軸を作ってみる
 
-start_mmdd = "3/1"
+start_mmdd = "3/22"
 start_yy = "20"
 
 i = mmddyyyy2date("#{start_mmdd}/20#{start_yy}")
@@ -42,12 +45,111 @@ while (i < now)
   i = i + 1
 end
 
-
-time_series_header = []
 m = {}
-start_i_j = 4
 max_row = 0
 max_x = 0
+max_y = 0
+######################################################################
+if (states_flag)
+  start_Date = Time.now
+if (/(\d+)\/(\d+)\/(\d\d)/ =~ start_date)
+  start_Date = Date.new("20#{$3}".to_i, $1.to_i, $2.to_i)
+end
+  base_count = 1
+  last_day = {}
+  days = {}
+  dir_name = "CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/"
+  Dir::foreach(dir_name) do |filename|
+    if (filename =~ /\.csv$/)
+      onedayData = {}
+      date = ""
+      CSV.foreach(dir_name + filename, "r:UTF-8") do |row|
+        /(\d\d)-(\d\d)-(\d\d\d\d).csv/.match(filename)
+        date = Date.new($3.to_i, $1.to_i, $2.to_i)
+        if (date >= start_Date - 6)
+          if (olderThan03212020(filename))
+            state = row[0]
+            country = row[1]
+            confirmed = row[3].to_i
+            if (confirmed.to_i >= base_count)
+              states.each{|s, c|
+                if (state == s && country == c)
+                  m_index = "#{s}"
+                  onedayData[m_index] = confirmed
+                end
+              }
+            end
+          else
+            county = row[1]
+            state = row[2]
+            country = row[3]
+            confirmed = row[7].to_i
+            states.each{|s, c|
+              if (state == s && country == c)
+                m_index = "#{s}"
+                # at first s, c だったら、テンポラリの合計に入れる
+                if (onedayData[m_index]  == nil)
+                  onedayData[m_index]  = confirmed
+                else
+                  onedayData[m_index]  = onedayData[m_index] + confirmed
+                end
+              end
+            }
+          end
+        end
+      end
+      # after March/22 check whether add to m[] or not
+      states.each{|s, c|
+        m_index = "#{s}"
+        d = date2mmdd(date)
+        if (onedayData[m_index] != nil && onedayData[m_index] >= base_count)
+          confirmed = onedayData[m_index]
+          if (last_day[m_index] == nil)
+            last_day[m_index] = 0
+            m[m_index] = []
+          else
+            last_day[m_index] = last_day[m_index] + 1
+          end
+          #g = [confirmed, "#{d_index}:#{d}"]
+          g = confirmed.to_i
+          m[m_index][last_day[m_index]] = g
+          if (max_x < last_day[m_index])
+            max_x = last_day[m_index]
+          end
+          if (max_y < confirmed)
+            max_y = confirmed
+          end
+        end
+      }
+    end
+  end
+  m_avg = {}
+  m.each { |a|
+    i = a[1].length - 1
+    s_avg =  "#{a[0]}_avg"
+    m_avg[s_avg] = [0]
+    while (i >= 1)
+      if (i > 6)
+        m_avg[s_avg][i] = (a[1][i] - a[1][i - 6]) / 7
+      else
+        m_avg[s_avg][i] = 0
+      end
+      m[a[0]][i] = a[1][i] - a[1][i - 1]
+      if (m[a[0]][i] < 0)
+        m[a[0]][i] = 0
+      end
+      i = i - 1
+    end
+  }
+  m_avg.each { |a|
+    m[a[0]] = a[1]
+  }
+end
+
+######################################################################
+
+time_series_header = []
+start_i_j = 4
 # XX Tokyo, Kanagawa daily new cases>
 CSV.foreach("time_series_covid19_confirmed_Japan.csv", "r:UTF-8") do |row|
   if (time_series_header == [])
@@ -96,7 +198,7 @@ CSV.foreach("time_series_covid19_confirmed_Japan.csv", "r:UTF-8") do |row|
 end
 
 
-
+######################################################################
 # XX 7 days avg.
      # --> by line
 
@@ -104,13 +206,33 @@ end
 header_str = []
 readHtml("daily-header.html", header_str)
 
-pp labels
+m.each{|a|
+  pref = a[0]
+  if (pref !~ /_avg/)
+    button = <<-EOS
+	<button id="addDataset%%PREF%%">Add Dataset %%PREF_LONG%%</button>
+	<button id="removeDataset%%PREF%%">Remove Dataset %%PREF_LONG%%</button>
+EOS
+    button = button.gsub(/%%PREF%%/, pref.gsub(/ /,""))
+    button = button.gsub(/%%PREF_LONG%%/, pref)
+    puts button
+  end
+}
+
 mid_str = []
 readHtml("daily-mid.html", mid_str)
+pp labels
+mid_str = []
+readHtml("daily-mid2.html", mid_str)
 
-color_table_daily = [ "Red", "Red",
-                      "Blue", "Blue",
-                    "Green", "Green"]
+color_table_daily = [ "Red", 
+                      "Blue",
+                      "Green", 
+                      "Orange",
+                      "Yellow",
+                      "purple",
+                      "grey",
+                    ]
 
 #  "Green", "Black", #"Cyan",
 #  "Orange", "Purple",
@@ -119,16 +241,24 @@ color_table_daily = [ "Red", "Red",
 #  "navy", "silver", "gray"]
 
   
-max_color_index = $color_table.length
+max_color_index = color_table_daily.length
 color_index = 0
 buttons = ""
+pref_color = {}
 m.each{|a|
-  pref = a[0]
-  color = color_table_daily[color_index % max_color_index].downcase
+  pref = a[0].gsub(/ /,"")
+  pref_long = a[0]
+  pref_short = pref.gsub(/_avg/,"")
+  if (pref_color[pref_short] == nil)
+    color = color_table_daily[color_index % max_color_index].downcase
+    color_index = color_index + 1
+    pref_color[pref_short] = color
+  else
+    color = pref_color[pref_short]
+  end
   
-  color_index = color_index + 1
   puts "var data#{pref} = "
-  puts "{label: '#{pref}',"
+  puts "{label: '#{pref_long}',"
   if (pref =~ /_avg/)
     puts "type: 'line',"
     puts "fill: false,"
@@ -142,10 +272,16 @@ m.each{|a|
   pp a[1]
   puts ","
   puts "			}; "
-  puts "barChartData.datasets.push(data#{pref});"
-  puts "var datasetFlag#{pref} = true;"
+  if (pref !~ /^NewYork/ && pref !~ /Washington/ && pref !~ /California/)
+    puts "barChartData.datasets.push(data#{pref});"
+  end
 
   if (pref !~ /_avg/)
+    if (pref !~ /^NewYork/)
+      puts "var datasetFlag#{pref} = true;"
+    else
+      puts "var datasetFlag#{pref} = false;"
+    end
   button =  <<-EOS
 		document.getElementById('addDataset%%PREF%%').addEventListener('click', function() {
 		if (datasetFlag%%PREF%% == false) {
@@ -157,15 +293,33 @@ m.each{|a|
 		});
 
 		document.getElementById('removeDataset%%PREF%%').addEventListener('click', function() {
-		if (datasetFlag%%PREF%% == true) {
-			barChartData.datasets.pop(data%%PREF%%_avg);
-			barChartData.datasets.pop(data%%PREF%%);
-			datasetFlag%%PREF%% = false;
-			window.myBar.update();
-			}
+                    if (datasetFlag%%PREF%% == true) {
+                        var l = barChartData.datasets.length;
+                        console.log(l);
+                        for (let i = 0; i < l ; i++) {
+                            console.log(i);
+                            console.log(barChartData.datasets[i].label);
+                            if (barChartData.datasets[i].label == "%%PREF_LONG%%") {
+                                barChartData.datasets.splice(i,1);
+                                break;
+                            }
+                        }
+                        var l = barChartData.datasets.length;
+                        console.log(l);
+                        for (let i = 0; i < l ; i++) {
+                            if (barChartData.datasets[i].label == "%%PREF_LONG%%_avg") 
+{
+                                barChartData.datasets.splice(i,1);
+                                break;
+                            }
+                        }
+         		datasetFlag%%PREF%% = false;
+                        window.myBar.update();
+		    }
 		});
 EOS
-  button = button.gsub(/%%PREF%%/, pref)
+  button = button.gsub(/%%PREF%%/, pref.gsub(/ /,""))
+  button = button.gsub(/%%PREF_LONG%%/, pref_long)
   buttons = buttons + button
   end
 }
