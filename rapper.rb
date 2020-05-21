124a04feaf17ae9ee7fb6c42f8f5c130acbd9a0e
Yoshiro MIHIRA <sanpei@sanpei.org>
2020-05-22 07:44:27 +0900

    add md5_csse_us and rename md5_csse to md5_csse_global

--- rapper.rb
+++ rapper.rb
@@ -22,17 +22,26 @@ def create_graph_CSSE(base_values)
   }
 end
 
-md5_csse_filename = "confirmed_global.md5"
-csv_csse_filename = "./CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+md5_csse_global_filename = "confirmed_global.md5"
+csv_csse_global_filename = "./CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
-csv_japan_filename = "time_series_covid19_confirmed_Japan.csv"
+md5_csse_us_filename = "confirmed_us.md5"
+csv_csse_us_filename = "./CSSEGISandData/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+
+
 md5_japan_filename = "time_series_covid19_confirmed_Japan.md5"
+csv_japan_filename = "time_series_covid19_confirmed_Japan.csv"
 
-md5_csse_old = ""
+md5_csse_global_old = ""
+md5_csse_us_old = ""
 md5_japan_old = ""
 create_md5_flag = nil
-if (!File.exist?(md5_csse_filename))
-  File.new(md5_csse_filename, "w")
+if (!File.exist?(md5_csse_global_filename))
+  File.new(md5_csse_global_filename, "w")
+  create_md5_flag = true
+end
+if (!File.exist?(md5_csse_us_filename))
+  File.new(md5_csse_us_filename, "w")
   create_md5_flag = true
 end
 if (!File.exist?(md5_japan_filename))
@@ -43,9 +52,14 @@ if (create_md5_flag == nil)
   sleep(rand(20))
 end
 
-File.open(md5_csse_filename,"r") do |mail|
+File.open(md5_csse_global_filename,"r") do |mail|
   mail.each_line do |oneline|
-    md5_csse_old = oneline
+    md5_csse_global_old = oneline
+  end
+end
+File.open(md5_csse_us_filename,"r") do |mail|
+  mail.each_line do |oneline|
+    md5_csse_us_old = oneline
   end
 end
 File.open(md5_japan_filename,"r") do |mail|
@@ -58,13 +72,22 @@ end
 #
 
 csv = ""
-File.open(csv_csse_filename,"r") do |csv_file|
+File.open(csv_csse_global_filename,"r") do |csv_file|
   csv_file.each_line do |oneline|
     csv = csv + oneline
   end
 end
 #
-md5_csse = Digest::MD5.new.update(csv).to_s
+md5_csse_global = Digest::MD5.new.update(csv).to_s
+
+csv = ""
+File.open(csv_csse_us_filename,"r") do |csv_file|
+  csv_file.each_line do |oneline|
+    csv = csv + oneline
+  end
+end
+#
+md5_csse_us = Digest::MD5.new.update(csv).to_s
 
 csv = ""
 File.open(csv_japan_filename,"r") do |csv_file|
@@ -84,18 +107,18 @@ if (md5_japan_old != md5_japan)
 end
 
 base_values = [150]
-if (md5_csse_old != md5_csse)
+if (md5_csse_global_old != md5_csse_global)
   create_graph_ww(base_values)
-  create_graph_CSSE(base_values)
-  File.open(md5_csse_filename, "w") do |io|
-    io.write md5_csse
+  File.open(md5_csse_global_filename, "w") do |io|
+    io.write md5_csse_global
   end
 end
 
-if (md5_japan_old != md5_japan || md5_csse_old != md5_csse)
+if (md5_japan_old != md5_japan || md5_csse_us_old != md5_csse_us)
+  create_graph_CSSE(base_values)
   system("/usr/local/bin/ruby daily-graph.rb > contents/sanpei3.github.io/covid19-daily.html")
-  File.open(md5_csse_filename, "w") do |io|
-    io.write md5_csse
+  File.open(md5_csse_us_filename, "w") do |io|
+    io.write md5_csse_us
   end
   File.open(md5_japan_filename, "w") do |io|
     io.write md5_japan
